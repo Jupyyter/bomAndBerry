806c96cd6e385c467ca737df896cfdb6cc6fd578
Cristi <raducea.matei2005@gmail.com>
2023-09-24 19:04:48 +0300

    rfhj

--- src/bullet.gd
+++ src/bullet.gd
@@ -1,15 +1,24 @@
 extends CharacterBody2D
 
-var windsize = Rect2(0,0,DisplayServer.window_get_size().x, DisplayServer.window_get_size().y)
+@onready var sprite2D =$Sprite2D
+@export var animatedSprite:AnimatedSprite2D
+
+var windsize:Rect2 = Rect2(0,0,DisplayServer.window_get_size().x, DisplayServer.window_get_size().y)
 var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.velocity.y += rng.randf_range(-0.8, 0.8)
-
-	
+	animatedSprite.visible = false
+	#self.get_node("hit").play() 
+func _physics_process(delta):
+	var collisioninfo:KinematicCollision2D = move_and_collide(self.velocity)
+	if collisioninfo && collisioninfo.get_collider().name == "bouse":
+		sprite2D.visible = false
+		animatedSprite.visible = true
+		animatedSprite.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position += self.velocity
-	if !windsize.has_point(self.position):
+	#self.position += self.velocity
+	if animatedSprite.frame == 4:
 		self.queue_free()
--- src/player.gd
+++ src/player.gd
@@ -7,11 +7,16 @@ var shooting:bool=false #used to determine when to switch to shooting sprites
 var flipped:bool=false #if the character is flipped
 var rotate:int=1
 var fireRate:int=100
+var animation_name:String
 var originalRotation:float=self.rotation_degrees
 @onready var animationPlayer =$AnimationPlayer
 @onready var collisionshape2D =$CollisionShape2D
 @onready var sprite2D =$Sprite2D
 @onready var rotationDelay =$Timer
+@export var bulletSwpawnCoords:Node2D
+@export var shootUp:Node2D
+@export var shootRight:Node2D
+@export var shootDown:Node2D
 var bulletDelay:int = Time.get_ticks_msec()
 
 
@@ -20,23 +25,17 @@ func _ready():
 
 
 func _process(delta):
+	animation_name=""
 	if Input.is_action_pressed("mouseLeft"):
 		shooting=true
 	else:
 		shooting=false
-	if Input.is_action_pressed("mouseLeft") && Time.get_ticks_msec() - bulletDelay >= fireRate:
-		var mouse_position:Vector2 = get_viewport().get_mouse_position()
-		var bullet:Node = preload("res://scenes/bullet.tscn").instantiate()
-		bullet.position.x = self.position.x + self.get_node("CollisionShape2D").get_shape().get_rect().size.x + bullet.get_node("CollisionShape2D").get_shape().get_rect().size.x
-		bullet.position.y = self.position.y + self.get_node("CollisionShape2D").get_shape().get_rect().size.y/2
-		bullet.velocity = (mouse_position - bullet.position).normalized() * 10
-		bullet.rotation = atan(bullet.velocity.y/bullet.velocity.x)
-		get_tree().get_root().add_child(bullet)
-		bulletDelay = Time.get_ticks_msec()
 	get_input()
 
 func _physics_process(delta):
 	move_and_slide()
+	if Input.is_action_pressed("mouseLeft") && Time.get_ticks_msec() - bulletDelay >= fireRate:
+		shoot()
 
 func get_input()->void:
 	var input_direction :Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -56,7 +55,7 @@ func get_input()->void:
 	var direction:Vector2 = (get_global_mouse_position() - global_position).normalized()
 	var degrees:float=vectorToDegrees(direction)
 	#determine which animation to play based on the angle
-	var animation_name:String = get_animation_name(degrees)
+	animation_name = get_animation_name(degrees)
 
 	animationPlayer.play(animation_name)
 
@@ -65,11 +64,13 @@ func get_input()->void:
 		if !flipped:
 			sprite2D.scale.x=-1
 			collisionshape2D.scale.x=-1
+			bulletSwpawnCoords.scale.x=-1
 			flipped=true
 	else:
 		if flipped:
 			sprite2D.scale.x=1
 			collisionshape2D.scale.x=1
+			bulletSwpawnCoords.scale.x=1
 			flipped=false
 
 #used for normal movement
@@ -123,6 +124,19 @@ func vectorToDegrees(vector: Vector2) -> float:
 
 	return angle_degrees
 
+func shoot()->void:
+	var mouse_position:Vector2 = get_viewport().get_mouse_position()
+	var bullet:Node = preload("res://scenes/bullet.tscn").instantiate()
+	if animation_name.begins_with("up"):
+		bullet.position=shootUp.global_position
+	elif animation_name.begins_with("right"):
+		bullet.position=shootRight.global_position
+	elif animation_name.begins_with("down"):
+		bullet.position=shootDown.global_position
+	bullet.velocity = (mouse_position - bullet.position).normalized() * 10
+	bullet.rotation = atan(bullet.velocity.y/bullet.velocity.x)
+	get_tree().get_root().add_child(bullet)
+	bulletDelay = Time.get_ticks_msec()
 
 func _on_timer_timeout():
 	rotate*=-1
